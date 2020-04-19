3dbda45f0227971d03f390661179582d66a7bd3f
kienkk <kienlangvai64@gmail.com>
2020-04-19 16:09:20 +0700

    commit lan 2

--- app/src/main/java/com/gametk/ccaro/ArtificialIntelligence.java
+++ app/src/main/java/com/gametk/ccaro/ArtificialIntelligence.java
@@ -2,4 +2,28 @@ package com.gametk.ccaro;
 
 public class ArtificialIntelligence {
 
+    private static int DX[] = {-1, 1, 0, 0, -1, -1, 1, 1};
+    private static int DY[] = {0, 0, -1, 1, -1, 1, -1, 1};
+
+    public static int checkWin(int[][] table, int x, int y){
+        for (int i = 0; i < x; i++)
+            for (int j = 0; j < y; j++)
+                if (table[i][j] != 0)
+                    for (int t = 0; t < 8; t++){
+                        int sum = 0;
+                        for (int k = 0; k < 5; k++){
+                            int u = i + k * DX[t];
+                            int v = j + k * DY[t];
+                            if ((u >= 0) && (u < x) && (v >= 0) && (v < y)){
+                                if (table[u][v] != table[i][j]) break;
+                                sum += table[u][v];
+                            }else break;
+                        }
+                        if (sum == 5) return 1;
+                        if (sum == - 5) return -1;
+                    }
+
+        return 0;
+    }
+
 }
--- app/src/main/java/com/gametk/ccaro/PlayActivity.java
+++ app/src/main/java/com/gametk/ccaro/PlayActivity.java
@@ -1,8 +1,11 @@
 package com.gametk.ccaro;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +17,8 @@ import android.widget.Toast;
 public class PlayActivity extends AppCompatActivity {
 
     private int step = 0;   // số bước đã đi
+    private int[] BX;
+    private int[] BY;
     private int[][] table;  // giá trị bảng
     private int x = 16;
     private int y = 11;// size table = x * y
@@ -22,9 +27,7 @@ public class PlayActivity extends AppCompatActivity {
     private LinearLayout layoutView;  // khu vực chơi
     private LinearLayout dong ;
     private ImageView[][] imageViews; // ImageView mỗi ô
-    private Button you1,you2;
-    private int DX[] = {-1, 1, 0, 0, -1, -1, 1, 1};
-    private int DY[] = {0, 0, -1, 1, -1, 1, -1, 1};
+    private Button you1,you2,resetView,datLai;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,13 +40,36 @@ public class PlayActivity extends AppCompatActivity {
 
         createTable();
 
+        resetView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetTable();
+            }
+        });
+
+        datLai.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onClick(View v) {
+                back();
+                if((step % 2) == 1){
+                    you2.setBackgroundColor(getColor(R.color.colorGreen));
+                    you1.setBackground(null);
+                }else {
+                    you2.setBackground(null);
+                    you1.setBackgroundColor(getColor(R.color.colorGreen));
+                }
+            }
+        });
+
         if (hasAI){
             you1.setText("YOU");
             you2.setText("CPU");
             // máy chơi
         } else {
-            you1.setText("YOU 1");
-            you2.setText("YOU 2");
+            you1.setText("YOU 1\n(X)");
+            you2.setText("YOU 2\n(O)");
+
             // người chơi
         }
 
@@ -55,7 +81,11 @@ public class PlayActivity extends AppCompatActivity {
         layoutView.setWeightSum(x);
         you1 = findViewById(R.id.you1);
         you2 = findViewById(R.id.you2);
+        resetView = findViewById(R.id.resetView);
+        datLai = findViewById(R.id.datLai);
         table = new int[x][y];
+        BX = new int[x*y];
+        BY = new int[x*y];
         imageViews = new ImageView[x][y];
     }
 
@@ -72,14 +102,27 @@ public class PlayActivity extends AppCompatActivity {
                 final int finalJ = j;
                 final int finalI = i;
                 imageViews[i][j].setOnClickListener(new View.OnClickListener() {
+                    @RequiresApi(api = Build.VERSION_CODES.M)
                     @Override
                     public void onClick(View v) {
 
                         if (table[finalI][finalJ] == 0){
+
+                            if((step % 2) == 0){
+                                you2.setBackgroundColor(getColor(R.color.colorGreen));
+                                you1.setBackground(null);
+                            }else {
+                                you2.setBackground(null);
+                                you1.setBackgroundColor(getColor(R.color.colorGreen));
+                            }
+
                             if (hasAI){
+                                step++;
                                 // cho AI chơi
                             }else {
                                 step++;
+                                BX[step - 1] = finalI;
+                                BY[step - 1] = finalJ;
                                 if ((step % 2) == 0){
                                     imageViews[finalI][finalJ].setBackgroundResource(R.drawable.o);
                                     table[finalI][finalJ] = -1;
@@ -88,11 +131,11 @@ public class PlayActivity extends AppCompatActivity {
                                     table[finalI][finalJ] = 1;
                                 }
                                 // check Win
-                                if (checkWin() == 1){
+                                if (ArtificialIntelligence.checkWin(table, x, y) == 1){
                                     Toast.makeText(getApplicationContext(),"X win",Toast.LENGTH_SHORT).show();
                                     resetTable();
                                 }
-                                if (checkWin() == -1){
+                                if (ArtificialIntelligence.checkWin(table, x, y) == -1){
                                     Toast.makeText(getApplicationContext(),"O win",Toast.LENGTH_SHORT).show();
                                     resetTable();
                                 }
@@ -117,31 +160,18 @@ public class PlayActivity extends AppCompatActivity {
         dong.setWeightSum(y);
     }
 
-    private int checkWin(){
-        for (int i = 0; i < x; i++)
-            for (int j = 0; j < y; j++)
-                if (table[i][j] != 0)
-                    for (int t = 0; t < 8; t++){
-                        int sum = 0;
-                        for (int k = 0; k < 5; k++){
-                            int u = i + k * DX[t];
-                            int v = j + k * DY[t];
-                            if ((u >= 0) && (u < x) && (v >= 0) && (v < y)){
-                                if (table[u][v] != table[i][j]) break;
-                                sum += table[u][v];
-                            }else break;
-                        }
-                        if (sum == 5) return 1;
-                        if (sum == - 5) return -1;
-                    }
-
-        return 0;
-    }
-
     private void resetTable(){
         Intent intent = new Intent(getApplicationContext(),PlayActivity.class);
         startActivity(intent);
         finish();
+    }
+
+    private void back(){
+        if(step > 0) {
+            imageViews[BX[step-1]][BY[step-1]].setBackgroundResource(R.drawable.trong);
+            table[BX[step-1]][BY[step-1]] = 0;
+            step = step - 1;
+        }
     }
 
 }
